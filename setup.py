142258f01e3b2a8ecce12e1d7156a766bfb366ec
amanullah20 <ullah.aman1@gmail.com>
2026-05-25 17:00:46 +0600

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.0',
+    author="Aman Ullah",
+    author_email="ullah.aman1@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
